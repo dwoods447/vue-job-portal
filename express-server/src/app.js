61ad72e4f000e8ab83336d398c805680a723f298
Demaria Woods <dpwoods@uh.edu>
2019-06-19 19:11:09 -0500

    Added build directory

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(bodyParser.json())
 app.use(cors());
-app.use('/', express.static(path.join(__dirname, '../../vue-client/')))
+app.use('/', express.static(path.join(__dirname, '../../vue-client/build')))
 
 require('./JobSeekerPassport')
 require('./EmployerPassport')
