51aab63b6bfb4fac6d251c4216d7ad1f3f88a08b
Demaria Woods <dpwoods@uh.edu>
2019-06-23 13:13:15 -0500

    troubleshooting uploads

--- express-server/src/models/index.js
+++ express-server/src/models/index.js
@@ -3,16 +3,16 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const config = require('../config/config.js')
 const db = {}
-// const sequelize = new Sequelize(
-//     config.db.database,
-//     config.db.user,
-//     config.db.password,
-//     config.db.options
-// )
+const sequelize = new Sequelize(
+    config.db.database,
+    config.db.user,
+    config.db.password,
+    config.db.options
+)
 
-const sequelize = new Sequelize(config.db.url, {
-    dialect: 'postgres'
-})
+// const sequelize = new Sequelize(config.db.url, {
+//     dialect: 'postgres'
+// })
     
 fs.readdirSync(__dirname)
 .filter((file)=>
