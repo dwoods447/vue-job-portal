145cae0a838395d925578bb7705f1161cd5e8e50
Demaria Woods <dpwoods@uh.edu>
2019-06-25 00:53:32 -0500

    toubleshooting view applicants - 12:53

--- vue-client/src/components/jobs/ViewApplicants.vue
+++ vue-client/src/components/jobs/ViewApplicants.vue
@@ -3,9 +3,11 @@
      <v-container>
         <h1>View Applicants:</h1>
        <v-layout row wrap v-if="!applicants">
-            <h2>No Applicants at this time. Check back later.</h2>
+         <v-flex>
+           <h2>No Applicants at this time. Check back later.</h2>
+         </v-flex>
        </v-layout>
-       <v-layout row wrap v-if="applicants">
+       <v-layout row wrap>
             <v-flex xs12 v-for="applicant in applicants" :key="applicant.id" style="max-width: 1200px; margin: 10px auto;">
               <v-toolbar color="primary">
                   <h2><span class="white--text">{{ applicant.Jobseeker.name }} - applying for <span>{{applicant.Job.type}}</span> </span>&nbsp;<strong><span class="white--text"> <span style="text-decoration: underl">{{applicant.Job.jobTitle}}</span> position</span></strong></h2>
