98940b4377034083fb27a365eb2536e0ad3e017f
Demaria Woods <dpwoods@uh.edu>
2019-08-25 20:28:44 -0500

    added url

--- vue-client/src/services/Api.js
+++ vue-client/src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import store from '../store'
 const instance = axios.create({
-  // baseURL: 'https://vue-job-portal.herokuapp.com',
-  baseURL: 'http://localhost:3000/',
+   baseURL: 'https://vue-job-portal.herokuapp.com',
+ // baseURL: 'http://localhost:3000/',
   headers: {
     Authorization: `Bearer ${store.state.token}`
   }
